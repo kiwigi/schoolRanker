fc75e273382e820406aada2a4f740447d8f34800
Martha Gil <you@example.com>
2020-11-11 11:36:40 -0700

    python code

--- dataGen.py
+++ dataGen.py
@@ -1,1 +1,20 @@
-print "AHHHHHHHHHHH"
+import random
+
+#This program will generate random data about students at a university
+#Will generate 100 data enteries 
+
+nameList = ["Cats", "Pizza", "Gaming","C4lg4ry","Monkey", "Dogs", "Mars","P-72","Pp"]
+universityName = "University of " + random.choice(nameList)
+
+
+#Data format: studentID, High school, GPA
+i = 0
+
+while i < 100:
+    studentID = random.randint(1111,9999)
+    highschoolName = random.choice(nameList) + " High School"
+    gpa =  round(random.uniform(0.0, 4.0),1)
+    dataEntry = str(studentID)+str(i) +" "+ highschoolName +" "+ str(gpa) #appending i to the ID will make sure it's unique
+    print(dataEntry)
+    i+=1
+
