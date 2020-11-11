71886b576c3197869a353cc86874b467776cff2d
Martha Gil <you@example.com>
2020-11-11 12:22:03 -0700

    Finished data denerator

--- dataGen.py
+++ dataGen.py
@@ -1,14 +1,14 @@
 import random
 
-#This program will generate random data about students at a university
-#Will generate 100 data enteries 
+#This program will generate random data about students at a university 
+#It will create a text file with 100 data enteries 
+#Data format: studentID, High school, GPA
 
-nameList = ["Cats", "Pizza", "Gaming","C4lg4ry","Monkey", "Dogs", "Mars","P-72","Pp"]
+nameList = ["Cats", "Pizza", "Gaming","C4lg4ry","Default Dance", "Dogs", "Mars","P-72","Pp","Coding","Epic","360 No-scope"]
 universityName = "University of " + random.choice(nameList)
 
-
-#Data format: studentID, High school, GPA
 i = 0
+f = open(universityName+ " " +'data.txt','w') #File will be named after a (randomly generated) university
 
 while i < 100:
     studentID = random.randint(1111,9999)
@@ -16,5 +16,8 @@ while i < 100:
     gpa =  round(random.uniform(0.0, 4.0),1)
     dataEntry = str(studentID)+str(i) +" "+ highschoolName +" "+ str(gpa) #appending i to the ID will make sure it's unique
     print(dataEntry)
+    f.write(dataEntry+'\n')
     i+=1
+
+
 
